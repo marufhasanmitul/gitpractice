87b8ab73d8115d99d9896f9ed2392d7545785a42
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-12-05 00:24:18 +0600

    Custom Page Added

---
+++ lib/custom_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomPage extends StatelessWidget {
+  const CustomPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Custom Page"),
+      ),
+    );
+  }
+}
