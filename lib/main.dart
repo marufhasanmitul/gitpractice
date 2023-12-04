cc7a525e50017799462063e37a608b4c78522191
Maruf Hasan <87512480+marufhasanmitul@users.noreply.github.com>
2023-12-05 01:23:19 +0600

    new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:untitled/custom_page.dart';
-import 'package:untitled/page_3.dart';
-import 'package:untitled/search_page.dart';
-
-
-void main(){
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class main extends StatelessWidget {
+  const main({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-     home: CustomPage(),
-    );
+    return const Placeholder();
   }
 }
